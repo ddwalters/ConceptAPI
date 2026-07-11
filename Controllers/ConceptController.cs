bc1c4a942e15bbddcf20e2f7cbef09ba4f01a4f2
Hiileike <ddwalters987@gmail.com>
2026-07-10 23:16:17 -0400

    Post/ Delete Controllers

--- Controllers/ConceptController.cs
+++ Controllers/ConceptController.cs
@@ -24,4 +24,29 @@ public class ConceptController : ControllerBase
 
         return Ok(concept);
     }
+
+    [HttpPost()]
+    public async Task<ActionResult<Concept>> PostConcept(Concept concept)
+    {
+        _context.Concepts.Add(concept);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(concept), new { Id = concept.Id }, concept);
+
+    }
+
+    [HttpDelete("{Id}")]
+    public async Task<ActionResult<Concept>> DeleteConcept(int id)
+    {
+        var concept = await _context.Concepts.FindAsync(id);
+
+        if (concept == null)
+            return NotFound(new { message = $"Concept with Id-{id} not found." });
+
+        _context.Concepts.Remove(concept);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+
+    }
 }
